a54205cca47b82b532a6f74b747e7c75f042f9f7
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-08-06 15:32:27 +0600

    Finding grading number

---
+++ gradingNumber.js
@@ -0,0 +1,106 @@
+/* You and your friends Tom, Jane, Peter and John got their final exam results. Your total
+score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56
+and John’s total score is 40. The grading chart is */
+
+let tom, jane, peter, john, riyadh;
+
+riyadh = 85;
+tom = 66;
+jane = 95;
+peter = 56;
+john = 40;
+
+/* checking condition for myself */
+if (riyadh >= 80 && riyadh <= 100) {
+    console.log("Riyadh Your Grate is A");
+}
+else if (riyadh >= 60 && riyadh <= 70) {
+    console.log("Riyadh Your Grate is B");
+}
+else if (riyadh >= 50 && riyadh <= 60) {
+    console.log("Riyadh Your Grate is C");
+}
+else if (riyadh >= 40 && riyadh <= 50) {
+    console.log("Riyadh Your Grate is D");
+}
+else {
+    console.log("Invalid");
+}
+
+
+
+
+/* checking condition for tom */
+if (tom >= 80 && tom <= 100) {
+    console.log("Tom Your Grate is A");
+}
+else if (tom >= 60 && tom <= 70) {
+    console.log("Tom Your Grate is B");
+}
+else if (tom >= 50 && tom <= 60) {
+    console.log("Tom Your Grate is C");
+}
+else if (tom >= 40 && tom <= 50) {
+    console.log("Tom Your Grate is D");
+}
+else {
+    console.log("Invalid");
+}
+
+
+
+/* checking condition for jane */
+
+if (jane >= 80 && jane <= 100) {
+    console.log("Jane Your Grate is A");
+}
+else if (jane >= 60 && jane <= 70) {
+    console.log("Jane Your Grate is B");
+}
+else if (jane >= 50 && jane <= 60) {
+    console.log("Jane Your Grate is C");
+}
+else if (jane >= 40 && jane <= 50) {
+    console.log("Jane Your Grate is D");
+}
+else {
+    console.log("Invalid");
+}
+
+
+/* checking condition for peter */
+if (peter >= 80 && peter <= 100) {
+    console.log("Peter Your Grate is A");
+}
+else if (peter >= 60 && peter <= 70) {
+    console.log("Peter Your Grate is B");
+}
+else if (peter >= 50 && peter <= 60) {
+    console.log("Peter Your Grate is C");
+}
+else if (peter >= 40 && peter <= 50) {
+    console.log("Peter Your Grate is D");
+}
+else {
+    console.log("Invalid");
+}
+
+
+
+/* checking condition for john */
+
+if (john >= 80 && john <= 100) {
+    console.log("John Your Grate is A");
+}
+else if (john >= 60 && john <= 70) {
+    console.log("John Your Grate is B");
+}
+else if (john >= 50 && john <= 60) {
+    console.log("John Your Grate is C");
+}
+else if (john >= 40 && john <= 50) {
+    console.log("John Your Grate is D");
+}
+else {
+    console.log("Invalid");
+}
